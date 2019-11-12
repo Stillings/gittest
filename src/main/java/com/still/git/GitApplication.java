cb9bd78a3940e90905abb90c6821cd90bda2641a
Stillings <liuwssf@163.com>
2019-11-12 23:09:15 +0800

    git测试提交

---
+++ src/main/java/com/still/git/GitApplication.java
@@ -0,0 +1,17 @@
+package com.still.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot启动方法
+ * LDD
+ */
+@SpringBootApplication
+public class GitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitApplication.class, args);
+    }
+
+}
